49978c195a483808c1db4352957dda6a65cd9759
opussf <github@zz9-za.com>
2025-03-18 12:55:23 -0700

    Test stubs
    Noli me tangere.

---
+++ test/test.lua
@@ -0,0 +1,26 @@
+#!/usr/bin/env lua
+
+require "wowTest"
+test.outFileName = "testOut.xml"
+test.coberturaFileName = "../coverage.xml"  -- to enable coverage output
+
+-- ParseTOC( "../src/QREvent.toc" )
+
+-- addon setup
+function test.before()
+	-- --Rested.eventFunctions = {}
+	-- Rested.filter = nil
+	-- Rested.reminders = {}
+	-- Rested.lastReminderUpdate = nil
+	-- Rested_options = {["showNumBars"] = 6}
+	-- Rested_restedState = {}
+	-- chatLog = {}
+	-- Rested.OnLoad()
+	-- Rested.ADDON_LOADED()
+	-- Rested.VARIABLES_LOADED()
+	-- --Rested.SaveRestedState()
+end
+function test.after()
+end
+
+test.run()

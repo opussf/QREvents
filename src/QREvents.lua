b7de6bec5ffcbadfe9b832285140257c64de90e9
opussf <github@zz9-za.com>
2025-03-18 22:13:07 -0700

    Give this a shot.
    A Fanatic is one who redoubles their effort when they have forgotten their aim.

--- src/QREvents.lua
+++ src/QREvents.lua
@@ -1,6 +1,19 @@
 QREVENTS_SLUG, QREvents = ...
 
+QREvents_events = {}
+
+function QREvents.Print( msg, showName)
+	-- print to the chat frame
+	-- set showName to false to suppress the addon name printing
+	-- if (showName == nil) or (showName) then
+		-- msg = COLOR_PURPLE..GOLDRATE_MSG_ADDONNAME.."> "..COLOR_END..msg
+	-- end
+	DEFAULT_CHAT_FRAME:AddMessage( msg )
+end
+
 function QREvents.OnLoad()
+	QREvents_Frame:RegisterEvent("CALENDAR_OPEN_EVENT")
+	QREvents_Frame:RegisterEvent("CALENDAR_ACTION_PENDING")
 end
 
 

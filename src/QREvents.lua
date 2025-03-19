3b1c956f572dc2d63bb3704dc729e32056260fd7
opussf <github@zz9-za.com>
2025-03-18 22:24:18 -0700

    QR addon, and cleaning code
    Everything originates from the seed of Determination.

--- src/QREvents.lua
+++ src/QREvents.lua
@@ -13,7 +13,51 @@ end
 
 function QREvents.OnLoad()
 	QREvents_Frame:RegisterEvent("CALENDAR_OPEN_EVENT")
-	QREvents_Frame:RegisterEvent("CALENDAR_ACTION_PENDING")
+	-- QREvents_Frame:RegisterEvent("CALENDAR_ACTION_PENDING")
 end
 
+function QREvents.CALENDAR_OPEN_EVENT(self, thing)
+	-- QREvents.Print( "CALENDAR_OPEN_EVENT( "..thing.." )" )
+	local info = C_Calendar.GetEventInfo()
+	-- QREvents.Print( info.title )
+	-- QREvents.Print( info.eventType )
+	local vcalTF = "%Y%m%dT%H%M%S"
+	local now = date( vcalTF )
+	-- Sigh...   Fix the 'fake' time table to a real lua time table
+	info.time.day = info.time.monthDay
+	info.time.wday = info.time.weekday
+	info.time.min = info.time.minute
 
+	local start = date( vcalTF, time(info.time) )
+
+	local vcal = {}
+	table.insert( vcal, "BEGIN:VCALENDAR" )
+	table.insert( vcal, "VERSION:2.0" )
+	table.insert( vcal, "BEGIN:VEVENT" )
+	table.insert( vcal, "DTSTAMP:"..now )
+	table.insert( vcal, "DTSTART:"..start )
+	table.insert( vcal, "DTEND:"..start )
+	table.insert( vcal, "TITLE:"..info.title )
+	table.insert( vcal, "SUMMARY:"..info.description )
+	table.insert( vcal, "LOCATION:Wow-realm" )
+	table.insert( vcal, "BEGIN:VALARM" )
+	table.insert( vcal, "TRIGGER:-P7D" )
+	table.insert( vcal, "ACTION:DISPLAY" )
+	table.insert( vcal, "END:VALARM" )
+	table.insert( vcal, "BEGIN:VALARM" )
+	table.insert( vcal, "TRIGGER:-P1D" )
+	table.insert( vcal, "ACTION:DISPLAY" )
+	table.insert( vcal, "END:VALARM" )
+	table.insert( vcal, "END:VEVENT" )
+	table.insert( vcal, "END:VCALENDAR" )
+
+	local tsnow = time()
+
+	QREvents_events["now"] = table.concat( vcal, "\n" )
+	QREvents_events[tsnow] = QREvents_events["now"]
+
+end
+
+function QREvents.CALENDAR_ACTION_PENDING(self, b )
+	QREvents.Print( "CALENDAR_ACTION_PENDING( "..(b and "True" or "False").." )" )
+end
